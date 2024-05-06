d97cf68bd93c9a50fa5be6106e32d0f03e290c39
FastRafool <107883223+FastRafool@users.noreply.github.com>
2024-05-06 15:13:40 -0500

    Update app.tsx

--- app.tsx
+++ app.tsx
@@ -1,9 +1,9 @@
 import {
-  HashRouter,
-  BrowserRouter,
-  Routes,
-  Route,
-  Outlet,
+    HashRouter,
+    BrowserRouter,
+    Routes,
+    Route,
+    Outlet,
 } from "react-router-dom";
 import { USE_BROWSER_ROUTER } from "./common/constants";
 import GlobalHeader from "./components/global-header";
@@ -11,29 +11,33 @@ import DashboardPage from "./pages/dashboard/dashboard-page";
 import ViewItemPage from "./pages/section1/view-item/view-item-page";
 import AllItemsPage from "./pages/section1/all-items/all-items-page";
 import AddItemPage from "./pages/section1/add-item/add-item-page";
+import NewsletterPage from "./pages/section2/newsletter/NewsletterPage";
 import NotFound from "./pages/not-found";
 import "./styles/app.scss";
 
 export default function App() {
-  const Router = USE_BROWSER_ROUTER ? BrowserRouter : HashRouter;
+    const Router = USE_BROWSER_ROUTER ? BrowserRouter : HashRouter;
 
-  return (
-    <div style={{ height: "100%" }}>
-      <Router>
-        <GlobalHeader />
-        <div style={{ height: "56px", backgroundColor: "#000716" }}>&nbsp;</div>
-        <div>
-          <Routes>
-            <Route index path="/" element={<DashboardPage />} />
-            <Route path="/section1" element={<Outlet />}>
-              <Route path="" element={<AllItemsPage />} />
-              <Route path="add" element={<AddItemPage />} />
-              <Route path="items/:itemId" element={<ViewItemPage />} />
-            </Route>
-            <Route path="*" element={<NotFound />} />
-          </Routes>
+    return (
+        <div style={{ height: "100%" }}>
+            <Router>
+                <GlobalHeader />
+                <div style={{ height: "56px", backgroundColor: "#000716" }}>&nbsp;</div>
+                <div>
+                    <Routes>
+                        <Route index path="/" element={<DashboardPage />} />
+                        <Route path="/section1" element={<Outlet />}>
+                            <Route path="" element={<AllItemsPage />} />
+                            <Route path="add" element={<AddItemPage />} />
+                            <Route path="items/:itemId" element={<ViewItemPage />} />
+                        </Route>
+                        <Route path="/section2" element={<Outlet />}>
+                            <Route path="" element={<NewsletterPage />} />
+                        </Route>
+                        <Route path="*" element={<NotFound />} />
+                    </Routes>
+                </div>
+            </Router>
         </div>
-      </Router>
-    </div>
-  );
+    );
 }
