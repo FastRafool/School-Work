a7c53755edea2f44fa7f8a9e23fa7070fa6a69a7
odudu321 <166865358+odudu321@users.noreply.github.com>
2024-05-10 16:38:19 -0500

    Delete navipanel.tsx

--- navipanel.tsx
+++
@@ -1,3 +0,0 @@
-curl -X POST https://pe7l3c89kl.execute-api.us-east-1.amazonaws.com/dev/subs \
-     -H "Content-Type: application/json" \
-     -d '{"email": "example@examp.com", "frequency": "weekly", "keywords": ["braket", "azure-quantu"]}'
